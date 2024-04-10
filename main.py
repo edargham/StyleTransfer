7f9af96712ab3bb5160fa4b9efdf0f0323ce65c1
Elio Khawand <Elio.khawand@gmail.com>
2024-04-11 01:49:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 import utils
 from config import config
 import tensorflow as tf
-import keras
+from tensorflow import keras
+from tensorflow.keras.applications import ResNet50
 
 from model.style_transfer_model import StyleContentModel
 from losses.style_content_loss import StyleContentLoss
 
 if __name__ == '__main__':
-  vgg = utils.load_vgg_19()
+    # Load the ResNet50 model pre-trained on ImageNet data
+    resnet = ResNet50(include_top=False, weights='imagenet')
 
-  style_layers = [
-    'block1_conv1',
-    'block2_conv1',
-    'block3_conv1',
-    'block4_conv1',
-    'block5_conv1'
-  ]
+    # Define style and content layers
+    style_layers = [
+        'conv1_conv',  # Corresponds to block1_conv1 in VGG19
+        # Add more layers as per the ResNet architecture
+    ]
 
-  content_layers=['block5_conv3']
+    content_layers = ['conv5_block3_out']  # Corresponds to block5_conv3 in VGG19
 
-  style_backbone = utils.get_submodel(vgg, style_layers + content_layers)
-  style_backbone.summary()
+    # Get the submodel from ResNet50
+    style_backbone = utils.get_submodel(resnet, style_layers + content_layers)
+    style_backbone.summary()
 
-  style_image = utils.load_img(config['style_image_path'])
-  content_image = utils.load_img(config['content_image_path'])
+    # Load style and content images
+    style_image = utils.load_img(config['style_image_path'])
+    content_image = utils.load_img(config['content_image_path'])
 
-  extractor = StyleContentModel(
-    model_backbone=style_backbone,
-    style_layers=style_layers,
-    content_layers=content_layers,
-    total_variation_weight=config['total_variation_weight'],
-  )
+    # Create the style content model
+    extractor = StyleContentModel(
+        model_backbone=style_backbone,
+        style_layers=style_layers,
+        content_layers=content_layers,
+        total_variation_weight=config['total_variation_weight'],
+    )
+
+    # Rest of the code remains the same...
+
 
   results = extractor(tf.constant(content_image))
   style_targets = extractor(style_image)['style']
