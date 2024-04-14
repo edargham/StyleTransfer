68363d611d7e1bbc0e5774a7cf75a748fedea34f
Elio Khawand <Elio.khawand@gmail.com>
2024-04-14 23:01:59 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ config = {
   'content_image_path': './content.jpeg',#r'C:\Users\edarg\Pictures\Ehmej.JPEG',
   'content_weight': 1e3,
   'style_weight': 1e-2,
-  'total_variation_weight': 30,
-  'epochs': 10,
+  'total_variation_weight': 0,
+  'epochs': 100,
   'steps_per_epoch': 100,
 }
