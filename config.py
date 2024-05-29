ab040ef6999b3d045671b0b3b5eda40288492d8b
Elias <edargham@hotmail.com>
2024-05-29 21:05:07 +0300

    Add Pretrain Notebook.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ config = {
   'content_image_path': './content.jpeg',#r'C:\Users\edarg\Pictures\Ehmej.JPEG',
   'content_weight': .8,
   'style_weight': .2,
-  'total_variation_weight': 3,
+  'total_variation_weight': 0,
   'epochs': 10,
   'steps_per_epoch': 1000,
 }
